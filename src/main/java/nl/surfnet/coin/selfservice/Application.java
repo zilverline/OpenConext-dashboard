34426a5e58372d1e194cbe2baea799644cdf384b
Hans Westerbeek <hans@enhance-ict.com>
2014-07-08 19:37:02 +0200

    Provide default logging config Configure appassemble plugin

--- src/main/java/nl/surfnet/coin/selfservice/Application.java
+++ src/main/java/nl/surfnet/coin/selfservice/Application.java
@@ -5,16 +5,24 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Will load configuration from whichever application.properties file is first on the classpath
+ */
 public class Application {
 
   private static final String PORT_ARG = "p";
-  public static final String APP_NAME = "selfservice";
 
-  public static void main(String[] args) {
+  public static final String APP_NAME = "selfservice";
+  public static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+
+  public static void main(String[] args) throws Exception{
+
     CommandLineParser parser = new GnuParser();
     HelpFormatter formatter = new HelpFormatter();
 
@@ -28,8 +36,10 @@ public class Application {
         formatter.printHelp(APP_NAME, options);
         return;
       } else if (cmd.hasOption(PORT_ARG)) {
-        System.out.println("Starting application on port " + cmd.getOptionValue("p"));
+
         Integer port = Integer.parseInt(cmd.getOptionValue(PORT_ARG));
+        LOGGER.info("Starting application on port {}", port);
+
         Server server = new Server(port);
 
         WebAppContext webAppContext = new WebAppContext();
@@ -44,6 +54,7 @@ public class Application {
         formatter.printHelp(APP_NAME, options);
       }
     } catch (Exception e) {
+      e.printStackTrace();
       System.err.println("Unexpected exception: " + e.getMessage());
       formatter.printHelp(APP_NAME, options);
     }
